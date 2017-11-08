aac824ff9a32031247642c5f85b463a7d2def711
Vikas Ranjan <vikasranjan008@gmail.com>
2017-11-08 18:32:34 +0530

    Symmetric/Mirror tree

--- src/CheckIfSymmetric.java
+++ src/CheckIfSymmetric.java
@@ -1,8 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * TODO: Document this class.
  */
 public class CheckIfSymmetric {
+    List<Integer> treeTraversalList = new ArrayList<>();
     public boolean isSymmetric(TreeNode root) {
-        return true;
+       if(null == root){
+           return true;
+       }
+
+       return checkMirror(root, root);
     }
+
+    private boolean checkMirror(final TreeNode root, final TreeNode root1) {
+        if(null == root && null == root1){
+            return true;
+        }
+
+        if(null != root && null != root1 && root.val == root1.val){
+            if(checkMirror(root.left, root1.right) && checkMirror(root.right , root1.left)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
